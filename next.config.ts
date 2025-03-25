e129cc2b0ca2d99b408f877cf22b2b714e5ee343
Fernando Moro <fernando.moro@radmas.com>
2025-03-25 14:21:53 +0100

    Add GitHub Pages support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/situm-poi-finder',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
